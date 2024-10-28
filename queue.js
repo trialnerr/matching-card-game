159de2f5ac7855200c301bae55d5015431c0cf33
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-10-28 15:14:42 -0400

    Refactor the queue to not add an element if it is already in the queue

--- queue.js
+++ queue.js
@@ -6,12 +6,22 @@ export class Queue{
   }
 
   push(item) {
-    this.queue.push(item); 
-    this.length++; 
+    if (!this.queue.includes(item)) {
+      this.queue.push(item); 
+      this.length++; 
+    }
   } 
 
   reset() {
     this.queue = []; 
     this.length = 0; 
+  }
+
+  dismountFirstTwo() {
+    const firstTwo = this.queue.slice(0, 2);
+    this.queue.shift(); 
+    this.queue.shift();
+    this.length -= 2;
+    return firstTwo;
   }
 }
